f979a7633f3bdb2c16d2d502c583e97114663621
kiheon0709 <hongkiheon@naver.com>
2025-03-28 16:15:43 +0900

    feat: 예약 기능 관련 컨트롤러 메서드 구현 및 경로 매핑 완료

--- src/main/java/com/example/hospitalreservation/controller/ReservationController.java
+++ src/main/java/com/example/hospitalreservation/controller/ReservationController.java
@@ -1,35 +1,52 @@
 package com.example.hospitalreservation.controller;
 
+import com.example.hospitalreservation.model.Reservation;
+import com.example.hospitalreservation.service.ReservationService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
+
 
 // TODO : 컨트롤러에 필요한 어노테이션을 작성해주세요.
 // TODO : 요청 경로는 templates를 참고하여 작성해주세요.
+@Controller
 public class ReservationController {
 
     // TODO : 주입 받아야 할 객체를 설정해주세요.
+    private ReservationService reservationService;
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
 
     // TODO : 필요한 어노테이션을 작성해주세요.
+    @GetMapping("/reservations")
     public String getReservations(Model model) {
         // TODO : 예약 메인 페이지를 가져오는 코드를 작성해주세요.
-        return null;
+        model.addAttribute("reservations", reservationService.getAllReservations());
+        return "index";
     }
 
     // TODO : 필요한 어노테이션을 작성해주세요.
+    @GetMapping("/reservations/new")
     public String showReservationForm() {
         // TODO : 예약하기 페이지를 가져오는 코드를 작성해주세요.
-        return null;
+        return "reservation_form";
     }
 
     // TODO : 필요한 어노테이션을 작성해주세요.
+    @PostMapping("reservations")
     public String createReservation(@RequestParam Long doctorId, @RequestParam Long patientId) {
         // TODO : 예약을 진행하는 코드를 작성해주세요.
-        return null;
+        reservationService.createReservation(doctorId, patientId, LocalDateTime.now());
+        return "redirect:/reservations";
     }
 
     // TODO : 필요한 어노테이션을 작성해주세요.
+    @PostMapping("reservations/delete/{id}")
     public String cancelReservation(@PathVariable Long id) {
         // TODO : 예약을 취소하는 코드를 작성해주세요.
-        return null;
+        reservationService.cancelReservation(id);
+        return "redirect:/reservations";
     }
 }
